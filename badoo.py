1ff9aafc2b1ae14e2cb35c2fe98efe6db8881068
Luciano da Silva Martins <zicadopv@gmail.com>
2020-01-01 09:34:28 -0300

    badoo.py - calcular_rabbit_ganhos.py

---
+++ badoo.py
@@ -0,0 +1,106 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as CondicaoExperada
+from selenium.common.exceptions import *
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.keys import Keys
+
+from time import sleep
+import os, random, re, json, sys
+from datetime import datetime
+
+
+class BadooWithSelenium:
+    def __init__(self):
+        chrome_options = webdriver.ChromeOptions()
+        # chrome_options.add_argument('--headless')
+        self.driver = webdriver.Chrome(options=chrome_options)
+        self.driver.set_window_size(1120, 550)
+        self.wait = WebDriverWait(
+            self.driver,
+            10,
+            poll_frequency=1,
+            ignored_exceptions=[
+                NoSuchElementException,
+                ElementNotVisibleException,
+                ElementNotSelectableException,
+            ],
+        )
+
+    @staticmethod
+    def type_like_a_person(sentence, single_input_field):
+        """ Essa função basicamente simulará a digitação de uma pessoa"""
+        print("going to start typing message into message share text area")
+        for letter in sentence:
+            single_input_field.send_keys(letter)
+            sleep(random.randint(1, 5) / 30)
+
+
+    def main(self, url, params):
+        driver = self.driver
+        driver.get(url)
+        xpath_input_email = '//input[@name="email"]'
+        input_email = self.wait.until(
+            CondicaoExperada.element_to_be_clickable(
+                (By.XPATH, xpath_input_email)
+            )
+        )
+        sleep(random.randint(5, 7) / 30)
+        input_email.clear()
+        input_email.send_keys(params['email'])
+        sleep(random.randint(5, 7) / 30)
+        xpath_input_password = '//input[@name="password"]'
+        input_password = self.wait.until(
+            CondicaoExperada.element_to_be_clickable(
+                (By.XPATH, xpath_input_password)
+            )
+        )
+        sleep(random.randint(5, 7) / 30)
+        input_password.clear()
+        input_password.send_keys(params['password'])
+        sleep(random.randint(15, 18) / 30)
+        xpath_button_login = "//button[@name='post']"
+        button_login = self.wait.until(
+            CondicaoExperada.element_to_be_clickable(
+                (By.XPATH, xpath_button_login)
+            )
+        )
+        sleep(random.randint(15, 17) / 30)
+        button_login.click()
+        sleep(random.randint(5, 7) / 30)
+        xpath_pessoas_perto = "//a[@href='/search']"
+        pessoas_perto = self.wait.until(
+            CondicaoExperada.element_to_be_clickable(
+                (By.XPATH, xpath_pessoas_perto)
+            )
+        )
+        pessoas_perto.click()
+        online = "(.//*[normalize-space(text()) and normalize-space(.)='Pessoas Perto'])[2]/following::div[7]"
+        WebDriverWait(driver, 10).until(esperar_pelo_elemento(driver,online))
+        driver.find_element_by_link_text("Pessoas Perto").click()
+        driver.find_element_by_xpath(
+            "(.//*[normalize-space(text()) and normalize-space(.)='Pessoas Perto'])[2]/following::div[7]"
+        ).click()
+        driver.find_element_by_xpath(
+            "(.//*[normalize-space(text()) and normalize-space(.)='Pessoas Perto'])[2]/following::div[7]"
+        ).click()
+        driver.find_element_by_xpath(
+            "(.//*[normalize-space(text()) and normalize-space(.)='Novos membros'])[2]/following::div[1]"
+        ).click()
+
+if __name__ == '__main__':
+    url = 'https://us1.badoo.com/pt/signin/?f=top'
+    params = {}
+    # params['email'] = input('Digite o e-mail:')
+    # params['password'] = input('Digite a senha:')
+    params['email'] = 'zicadopv@terra.com.br'
+    params['password'] = '222222'
+    bd = BadooWithSelenium()
+    bd.main(url, params)
+    # if len(params['email']) < 1:
+    #     print('Digite e-mail/senha para acessar o site!!')
+    #     sys.exit(0)
+    # else:
+    #     print(params)
+    #     bd.main(url, params)
---
+++ calcular_rabbit_ganhos.py
@@ -0,0 +1,144 @@
+from requests import get, post, Session
+from pprint import pprint
+import datetime, re
+from time import sleep
+
+def variables_to_access_site(token):
+    cookies = {
+        '_ga': 'GA1.2.581327304.1556565197',
+        '_gid': 'GA1.2.1016363647.1556565197',
+        'a': 'e7635e19d341',
+        'a-e7635e19d341': 'G2k2G6YDgcrfGnZ2ExtwIWDP6JQbLKDTjdUqAlKitMst-9ZVzmGPb3k48',
+        'amplitude_idrabbiit.com': 'eyJkZXZpY2VJZCI6ImE4OTQyMjQxLTU4ZjAtNDgwMS1hNzhhLWUzMzJmNjU5MzVlYlIiLCJ1c2VySWQiOm51bGwsIm9wdE91dCI6ZmFsc2UsInNlc3Npb25JZCI6MTU1NjU2NTIxNDk1NSwibGFzdEV2ZW50VGltZSI6MTU1NjU2NTQzODAxNywiZXZlbnRJZCI6NCwiaWRlbnRpZnlJZCI6MCwic2VxdWVuY2VOdW1iZXIiOjR9',
+        'intercom-session-686c4e3617bad5cdb7e55dfaaace8b455742041c': 'SzQxV0xXci9LT2JQbzg1SVFabGlTZldpLzRZVk5iMUZhNThhak85azBDekJhcG13RnpVTEg3dkpCZmJGa054WS0tZkJKUmdKTEFHNXQ1Y01OeTN6VVJlQT09--fe775dde61c7fa780941c82201b7d1893d3bd0a9',
+    }
+
+    # Quando for usar tem q entrar no site, e atualizar esse authorization pelo https://curl.trillworks.com/#python
+    # headers = {
+    #     'Authorization': f'Bearer {token}',
+    #     'Accept-Encoding': 'gzip, deflate, br',
+    #     'Accept-Language': 'pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7,vi;q=0.6',
+    #     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36',
+    #     'Accept': 'application/json, text/plain, */*',
+    #     'Referer': 'https://app.rabbiit.com/a/e7635e19d341/',
+    #     'Connection': 'keep-alive',
+    # }
+
+    headers = {'Authorization': f'Bearer {token}'}
+
+    # params = {
+    #     'date_execution_end': '2019-12-31',
+    #     'date_execution_start': '2019-01-01',
+    #     'opt_sort_by': 'date_execution',
+    #     'opt_sort_direction': 'asc',
+    # }
+    url = 'https://app.rabbiit.com/api/v1/reports/detailed'
+    resultado = get(url, headers=headers, cookies=cookies)
+    return resultado
+
+def get_date(data):
+    try:
+        if ':' in data:
+            sep = ':'
+        else:
+            sep = '-'
+        ano, mes, dia = [int(i) for i in data.split(sep)]
+        return ano, mes, dia
+        # return date(ano, mes, dia)
+    except:
+        return None
+
+def get_time(time):
+    try:
+        hours, minutes, seconds = [int(i) for i in time.split(':')]
+        return datetime.timedelta(hours=hours, minutes=minutes, seconds=seconds)
+        # return hours, minutes, seconds
+    except:
+        return None
+
+def get_projects_in_rabbiit():
+    projects = []
+    for project in result['data']:
+        name ='-'.join((str(project['project_id']),project['project_name']))
+        projects.append(name)
+    print(set(projects))
+
+def logar(html, params):
+    # r = post(html, data=params)
+    session = Session()
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36'
+    }
+    response = session.post(html, data=params, headers=headers)
+    resul = session.get('https://app.rabbiit.com/a/e7635e19d341/#/projects/3')
+    resposta = re.findall('Backup', resul.text)
+    return resul
+
+
+def login():
+    params = {
+        'email': 'zicadopv@gmail.com',
+        'password': '222222'
+    }
+    html = 'https://app.rabbiit.com/api/v1/auth'
+    token = get(html, params).json()['token']
+    return token
+
+def acessar_pagina():
+    # url = 'https://app.rabbiit.com/a/e7635e19d341/#/projects/3'
+    url = 'https://app.rabbiit.com/api/v1/reports/detailed'
+    token = login()
+    headers = {'Authorization': f'token {token}'}
+    response = get(url, headers=headers)
+    return response.content
+
+def main():
+    soma = 0.0
+    today = datetime.date.today()
+    # project_id = 7
+    project_name = 'FreelaComAndré'
+    token = login()
+    response = variables_to_access_site(token)
+    result = response.json()['data']
+    totalHoras = datetime.timedelta(hours=0, minutes=0, seconds=0)
+    # day_today, month_today, year_today = get_date(today)
+    if today.month == 1:
+        month_previous = 12
+    else:
+        month_previous = today.month - 1
+    # print(month_previous)
+    # return
+    day_initial = '08'
+    day_final = '07'
+    # date_initial = day_initial/month_previous/year
+    # date_final = day_final/today.month/year
+    for projeto in result:
+        get_project_name = projeto['project_name']
+        print(get_project_name)
+        if 'FreelaComAndré' in get_project_name:
+            year_job, month_job, day_job = get_date(projeto['date_execution'])
+            print(year_job, month_job, day_job, month_previous)
+            if month_job == month_previous:
+                soma += float(projeto['rate_total'])
+                totalHoras += get_time(projeto['time_total'])
+                print(f"{day_job}, {month_job}, \
+                {projeto['time_start']}, \
+                {projeto['time_end']}, \
+                {projeto['time_total']}, \
+                {projeto['rate_total']} {round(soma, 3)} {totalHoras}")
+                sleep(1)
+    print(f' \
+          No total de {totalHoras} horas tive ganho de: R$ {round(soma, 3)} reais'
+    )
+
+
+if __name__ == '__main__':
+    mes = [1,2,3,4,5,6,7,8,9,10,11,12]
+    try:
+        # token = login()
+        # response = variables_to_access_site(token)
+        # acessar_pagina()
+        # result = response.json()
+        main()
+    except Exception as err:
+        print(f'Error..: {err}')
