75744db555bc3196ab3d1676befe49e0a11abfa6
Luciano Martins <zicadopv@gmail.com>
2020-09-23 15:36:37 -0300

    n alterações

---
+++ gazeta_do_povo.py
@@ -0,0 +1,6 @@
+from requests import get
+
+
+url = 'https://www.gazetadopovo.com.br/vozes/madeleine-lacsko/preso-homem-que-abusou-sexualmente-de-dezenas-de-criancas-usando-perfis-falsos/'
+html = get(url)
+print(html.content)
